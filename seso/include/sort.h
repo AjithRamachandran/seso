4d12e7518e76a1e1c593017033b8fa7b972a0b85
Ajith Ramachandran <ajithar204@gmail.com>
2021-05-31 01:32:02 +0530

    setup project for adding sorting algorithms

---
+++ seso/include/sort.h
@@ -0,0 +1,8 @@
+#ifndef SORT_H
+#define SORT_H
+
+int sort(double *arr, int len, char* algorithm) {
+    return 0;
+}
+
+#endif //SORT_H
--- seso/seso.c
+++ seso/seso.c
@@ -1,7 +1,35 @@
 #include <Python.h>
 #include <numpy/arrayobject.h>
 
+#include "include/sort.h"
+
+static PyObject* seso_sort(PyObject *self, PyObject *args) {
+    PyObject *arg=NULL, *arr=NULL;
+    double *cArr;
+    npy_intp dims[3];
+    char* algorithm = "bubblesort"; // bubblesort is the default sorting for now
+
+    if (!PyArg_ParseTuple(args, "O|s", &arg, &algorithm))
+        return NULL;
+
+    arr = PyArray_FROM_OTF(arg, NPY_DOUBLE, NPY_IN_ARRAY);
+
+    PyArray_AsCArray(&arr, (void *)&cArr, dims, PyArray_NDIM(arr), PyArray_DescrFromType(NPY_DOUBLE));
+
+    int err = sort(cArr, dims[0], algorithm);
+    
+    if(err==1) {
+        PyErr_SetString( PyExc_ValueError, "specified sort type not available");
+        Py_DECREF(arr);
+        return NULL;
+    } else {
+        Py_DECREF(arr);
+        return PyArray_Return(arr);
+    }
+}
+
 static PyMethodDef seso_methods[] = {
+    {"sort", seso_sort, METH_VARARGS, "sorting algorithms" },
     {NULL, NULL, 0, NULL}
 };
 
