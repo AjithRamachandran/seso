f2caef9f8e2044520f9d8139f741976b0cd7f344
Ajith Ramachandran <ajithar204@gmail.com>
2021-05-31 01:43:30 +0530

    added bubble sort and selection sort

--- seso/include/sort.h
+++ seso/include/sort.h
@@ -1,7 +1,16 @@
 #ifndef SORT_H
 #define SORT_H
 
-int sort(double *arr, int len, char* algorithm) {
+#include "bubblesort.h"
+
+int sort(double *arr, int len, char *algorithm) {
+    if (strcmp(algorithm, "bubblesort") == 0) {
+        bubbleSort(arr, len);
+    } else if (strcmp(algorithm, "selectionsort") == 0) {
+        bubbleSort(arr, len);
+    } else {
+        return 1;
+    }
     return 0;
 }
 
