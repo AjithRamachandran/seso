7454ffadb5ac82c2a20d6865bf10a3b9d692c241
Ajith Ramachandran <ajithar204@gmail.com>
2021-05-31 01:56:30 +0530

    added insertion sort and quick sort

---
+++ seso/include/insertionsort.h
@@ -0,0 +1,17 @@
+#ifndef INSERTION_SORT_H
+#define INSERTION_SORT_H
+
+void insertionSort(double *arr, int size) {
+    int i, j, key;
+    for (i = 1; i < size; i++) {
+        j = i - 1;
+        key = arr[i];
+        while (j >= 0 && key < arr[j]) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+#endif //INSERTION_SORT_H
--- seso/include/sort.h
+++ seso/include/sort.h
@@ -2,12 +2,19 @@
 #define SORT_H
 
 #include "bubblesort.h"
+#include "selectionsort.h"
+#include "insertionsort.h"
+#include "quicksort.h"
 
 int sort(double *arr, int len, char *algorithm) {
     if (strcmp(algorithm, "bubblesort") == 0) {
         bubbleSort(arr, len);
     } else if (strcmp(algorithm, "selectionsort") == 0) {
-        bubbleSort(arr, len);
+        selectionSort(arr, len);
+    } else if (strcmp(algorithm, "insertionsort") == 0) {
+        insertionSort(arr, len); 
+    } else if (strcmp(algorithm, "quicksort") == 0) {
+        quickSort(arr, 0, len-1); 
     } else {
         return 1;
     }
