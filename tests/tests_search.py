85aaa46e09a097f82cb6e0d1cecc29dd96ed63b5
Ajith Ramachandran <ajithar204@gmail.com>
2021-05-31 14:00:48 +0530

    added searching algorithms
    added linear search, binary search, exponential search, fibonacci search, interpolation search, jump search, ternary search.

--- tests/tests_search.py
+++ tests/tests_search.py
@@ -10,5 +10,28 @@ class TestSearchAlgorithms(unittest.TestCase):
         self.val = 5
         self.true = 2
 
+    def test_linear_search(self):
+        self.assertEqual(seso.search(self.arr, self.val, 'linearsearch'), self.true)
+
+    def test_binary_search(self):
+        self.assertEqual(seso.search(self.arr, self.val, 'binarysearch'), self.true)
+
+    def test_jump_search(self):
+        self.assertEqual(seso.search(self.arr, self.val, 'jumpsearch'), self.true)
+
+    def test_interpolation_search(self):
+        arr = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]) # interpolation search works only on sorted array
+        true = 5
+        self.assertEqual(seso.search(arr, self.val, 'interpolationsearch'), true)
+
+    def test_exponential_search(self):
+        self.assertEqual(seso.search(self.arr, self.val, 'exponentialsearch'), self.true)
+
+    def test_fibonacci_search(self):
+        self.assertEqual(seso.search(self.arr, self.val, 'fibonaccisearch'), self.true)
+
+    def test_ternary_search(self):
+        self.assertEqual(seso.search(self.arr, self.val, 'ternarysearch'), self.true)
+
 if __name__ == '__main__':
     unittest.main()
